fb1b918241a3bf7432e43c33099be1856578afc5
yassinechouket <medyassinechouket.2@gmail.com>
2026-01-16 11:41:15 +0100

    REST API

--- database/index.ts
+++ database/index.ts
@@ -1,10 +1,7 @@
-/**
- * Database models export
- * Central export point for all database models
- * 
- * Usage:
- * import { Event, Booking } from '@/database';
- */
+// Database models exports
+export { default as Event } from './event.model';
+export { default as Booking } from './booking.model';
 
-export { default as Event, type IEvent } from './event.model';
-export { default as Booking, type IBooking } from './booking.model';
+// TypeScript interfaces exports
+export type { IEvent } from './event.model';
+export type { IBooking } from './booking.model';
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    cacheComponents: true,
+    images: {
+      remotePatterns: [
+          {
+              protocol: 'https',
+              hostname: 'res.cloudinary.com',
+          }
+      ]
+    },
 };
 
 export default nextConfig;
