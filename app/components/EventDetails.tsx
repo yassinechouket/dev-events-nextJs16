0f491733e58ecd4ac2c79d86e4953c0deb9fc464
yassinechouket <medyassinechouket.2@gmail.com>
2026-01-17 22:01:26 +0100

    compo 2

--- app/components/EventDetails.tsx
+++ app/components/EventDetails.tsx
@@ -1,11 +1,36 @@
 import {cacheLife} from "next/cache";
 import { notFound } from "next/navigation";
+import Image from "next/image";
 
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 
-  (process.env.VERCEL_URL 
-    ? `https://${process.env.VERCEL_URL}` 
-    : 'http://localhost:3000');
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
+
+
+const EventDetailItem = ({ icon, alt, label }: { icon: string; alt: string; label: string; }) => (
+    <div className="flex-row-gap-2 items-center">
+        <Image src={icon} alt={alt} width={17} height={17} />
+        <p>{label}</p>
+    </div>
+)
+
+const EventAgenda = ({ agendaItems }: { agendaItems: string[] }) => (
+    <div className="agenda">
+        <h2>Agenda</h2>
+        <ul>
+            {agendaItems.map((item) => (
+                <li key={item}>{item}</li>
+            ))}
+        </ul>
+    </div>
+)
+
+const EventTags = ({ tags }: { tags: string[] }) => (
+    <div className="flex flex-row gap-1.5 flex-wrap">
+        {tags.map((tag) => (
+            <div className="pill" key={tag}>{tag}</div>
+        ))}
+    </div>
+)
 const EventDetails = async ({ params }: { params: Promise<{ slug: string }> }) => {
     'use cache'
     cacheLife('hours');
@@ -34,14 +59,47 @@ const EventDetails = async ({ params }: { params: Promise<{ slug: string }> }) =
         console.error('Error fetching event:', error);
         return notFound();
     }
+    const { description, image, overview, date, time, location, mode, agenda, audience, tags, organizer } = event;
 
   
   return (
-    <>
-        <h1>Event Details :{slug}</h1>
-        <p>{event.description}</p>
-        <img src={event.image} alt={event.title} />
-    </>
+    
+        <section id="event">
+            <div className="header">
+                <h1>Event Description</h1>
+                <p>{description}</p>
+            </div>
+
+            <div className="details">
+                {/*    Left Side - Event Content */}
+                <div className="content">
+                    <Image src={image} alt="Event Banner" width={800} height={800} className="banner" />
+
+                    <section className="flex-col-gap-2">
+                        <h2>Overview</h2>
+                        <p>{overview}</p>
+                    </section>
+
+                    <section className="flex-col-gap-2">
+                        <h2>Event Details</h2>
+                        <EventDetailItem icon="/icons/calendar.svg" alt="calendar" label={date} />
+                        <EventDetailItem icon="/icons/clock.svg" alt="clock" label={time} />
+                        <EventDetailItem icon="/icons/pin.svg" alt="pin" label={location} />
+                        <EventDetailItem icon="/icons/mode.svg" alt="mode" label={mode} />
+                        <EventDetailItem icon="/icons/audience.svg" alt="audience" label={audience} />
+                    </section>
+                    <EventAgenda agendaItems={agenda} />
+
+                    <section className="flex-col-gap-2">
+                        <h2>About the Organizer</h2>
+                        <p>{organizer}</p>
+                    </section>
+
+                    <EventTags tags={tags} />
+                </div>
+            </div>
+        </section>
+    
     
 
   )
